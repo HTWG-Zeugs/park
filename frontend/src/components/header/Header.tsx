c9ca0636becffbeaa3538b536173220e5adbde58
LukasEEH <lukas.epple25@web.de>
2024-12-18 20:35:29 +0100

    improve application header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -5,17 +5,21 @@ import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Button, Container, Menu, MenuItem } from "@mui/material";
+import { Button, Container, Menu, MenuItem, Tooltip } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "src/services/AuthContext";
 import { auth } from "src/services/FirebaseConfig";
 import { useTranslation } from "react-i18next";
-
+import HomeIcon from '@mui/icons-material/Home';
+import LogoutIcon from '@mui/icons-material/Logout';
+import LoginIcon from '@mui/icons-material/Login';
+import TranslateIcon from '@mui/icons-material/Translate';
+import Flag from 'react-flagkit';
 
 export default function Header() {  
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
   const pages = [
-    { text: t("component_header.home"), href: "/home" },
+    { text: <HomeIcon/>, href: "/home" },
     { text: t("component_header.occupancy"), href: "/occupancy"},
     { text: t("component_header.garages"), href: "/garages" },
     { text: t("component_header.defects"), href: "/defects" },
@@ -27,9 +31,9 @@ export default function Header() {
     null
   );
 
-
   const { isAuthenticated, logout } = useAuth();
   const [userEmail, setUserEmail] = useState<string | null>(null);
+  const [language, setLanguage] = useState<string>('GB');
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
@@ -50,6 +54,16 @@ export default function Header() {
     navigate(to);
     setAnchorElNav(null);
   };
+
+  const toggleLanguage = () => {
+    if (language == 'GB') {
+      setLanguage('DE');
+      i18n.changeLanguage('de')
+    } else {
+      setLanguage('GB')
+      i18n.changeLanguage('en')
+    }
+  }
 
   const handleLogout = async () => {
     await logout();
@@ -156,27 +170,33 @@ export default function Header() {
                 </Button>
               ))}
             </Box>
-
+            <Button style={{marginRight: "10px"}}
+              onClick={() => toggleLanguage()}
+              sx={{ color: "white", fontWeight: "bold" }}>
+              <TranslateIcon style={{marginRight: "5px"}}/><Flag country={language} />
+            </Button>
             {isAuthenticated && userEmail && (
               <Box sx={{ display: "flex", alignItems: "center" }}>
                 <Typography sx={{ color: "white", mr: 2 }}>
                   {userEmail}
                 </Typography>
-                <Button
-                  onClick={handleLogout}
-                  sx={{ color: "white", fontWeight: "bold" }}
-                >
-                  {t("component_header.sign_out")}
-                </Button>
+                <Tooltip title={t("component_header.sign_out")}>
+                  <IconButton
+                    onClick={handleLogout}
+                    sx={{ color: "white", fontWeight: "bold" }}>
+                    <LogoutIcon/>
+                  </IconButton>
+                </Tooltip>
               </Box>
             )}
             {!isAuthenticated && (
-              <Button
-                onClick={() => navigate("/sign-in")}
-                sx={{ color: "white", fontWeight: "bold" }}
-              >
-                {t("component_header.sign_in")}
-              </Button>
+              <Tooltip title={t("component_header.sign_in")}>
+                <IconButton
+                  onClick={() => navigate("/sign-in")}
+                  sx={{ color: "white", fontWeight: "bold" }}>
+                  <LoginIcon/>
+                </IconButton>
+              </Tooltip>
             )}
           </Toolbar>
         </Container>
