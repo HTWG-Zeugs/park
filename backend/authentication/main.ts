98a748335790e181c57cdae889129e8514a3131b
Lukas Benner <lukasbenner@mailbox.org>
2025-01-13 16:14:24 +0100

    hot fix cors

--- backend/authentication/main.ts
+++ backend/authentication/main.ts
@@ -15,11 +15,6 @@ const port = Config.PORT;
 app.use(express.json());
 
 app.use(cors());
-app.use(cors({
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
 
 const repo: Repository = FirestoreRepository.getInstance();
 const userService: UserService = UserService.getInstance(repo);
