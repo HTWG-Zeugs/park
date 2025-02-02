162301d1989803fb67f09caf4f2191a3e1689240
Lukas Benner <lukasbenner@mailbox.org>
2025-02-02 11:43:33 +0100

    update load testing script

--- load-testing/once-in-a-lifetime-test-free.js
+++ load-testing/once-in-a-lifetime-test-free.js
@@ -3,35 +3,19 @@ import { check, sleep } from 'k6';
 
 export const options = {
    stages: [
-      { duration: '5m', target: 2000 },
-      { duration: '10m', target: 2000 },
-      { duration: '5m', target: 0 },
+      { duration: '1m', target: 1000 },
+      { duration: '10m', target: 1000 },
+      { duration: '1m', target: 0 },
     ],
 };
 
 export default function () {
-  const garageId = "bdeec1e1-f6cf-47e9-a4c9-a340a288bc54"
-  const enterpriseUrl = "https://mercedes.park-staging-app.tech/parking";
-  const freeUrl = "https://free.park-staging-app.tech/parking";
-  const baseUrl = Math.random() < 0.3 ? freeUrl : enterpriseUrl;
+  const garageId = "1bac896e-443a-4785-a0a6-459a12930baf";
 
   let response;
 
   do {
-    sleep(1);
-    const url = baseUrl + `/garage/enter/${garageId}`;
-    response = http.post(url);
-  } while (response.status != 200);
-  check(response, {
-    'status is 200': (r) => r.status === 200,
-    'response time < 400ms': (r) => r.timings.duration < 400,
-  });
-  const ticketId = response.body;
-
-
-  do {
-    sleep(1);
-    const url = baseUrl + `/garage/handlePayment/${ticketId}`;
+    const url = `https://free.park-staging-app.tech/parking/garare/enter/${garageId}`;
     response = http.post(url);
   } while (response.status != 200);
   check(response, {
@@ -39,9 +23,20 @@ export default function () {
     'response time < 400ms': (r) => r.timings.duration < 400,
   });
 
+  const ticketId = response.data();
+
+
   do {
-    sleep(1);
-    const url = baseUrl + `/garage/exit/${ticketId}`;
+    const url = `https://free.park-staging-app.tech/parking/garage/handlePayment/${ticketId}`;
+    response = http.post(url);
+  } while (response.status != 200);
+  check(response, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 400ms': (r) => r.timings.duration < 400,
+  });
+
+  do {
+    const url = `https://free.park-staging-app.tech/parking/garage/exit/${ticketId}`;
     response = http.post(url);
   } while (response.status != 200);
   check(response, {
@@ -49,5 +44,6 @@ export default function () {
     'response time < 400ms': (r) => r.timings.duration < 400,
   });
   
-  sleep(Math.random() * 5);
+
+  sleep(1);
 }
